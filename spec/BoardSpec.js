49d6304ea80bdaf6ceb773f0b133382a391809e3
Kat Hicks <kmhicks92@gmail.com>
2017-03-09 12:02:08 +0000

    Added grid property to board

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -3,13 +3,23 @@
 describe('Board', function() {
 
   var board;
+  var grid;
 
   beforeEach(function() {
     board = new Board();
+    grid = [[[],[],[]], [[],[],[]], [[],[],[]]];
   });
 
   it('should be a defined object', function() {
     expect(board instanceof(Board)).toBe(true);
+  });
+
+  it('should have a property for grid', function() {
+    expect(board.grid).toBeDefined();
+  });
+
+  it('should have the correct property for grid', function() {
+    expect(board.grid).toEqual(grid);
   });
 
 });
