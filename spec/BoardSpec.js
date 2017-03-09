6b8b7e9e9e8ea1576c472b83b09319089ed004ee
Kat Hicks <kmhicks92@gmail.com>
2017-03-09 11:54:50 +0000

    Added test to check that Board object exists

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -2,4 +2,14 @@
 
 describe('Board', function() {
 
+  var board;
+
+  beforeEach(function() {
+    board = new Board();
+  });
+
+  it('should be a defined object', function() {
+    expect(board instanceof(Board)).toBe(true);
+  });
+
 });
