4e71db3d5b9e48915474a4a1e2b5cb77007a0346
Kat Hicks <kmhicks92@gmail.com>
2017-03-09 11:13:08 +0000

    Expect player to have a name property

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -2,4 +2,18 @@
 
 describe('Player', function() {
 
+  var player;
+
+  beforeEach(function() {
+    player = new Player("Kat");
+  });
+
+  it('should be a defined object', function() {
+    expect(player instanceof(Player)).toBe(true);
+  });
+
+  it('should have a property for the board', function() {
+    expect(player.name).toBeDefined();
+  });
+
 });
